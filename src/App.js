d9905ebfbd3bcd6e85c24691cd8f83021d686b5f
Kipkoech78 <lkipkoech@kabarak.ac.ke>
2024-09-03 19:03:56 +0300

    first commit of front end side of task 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Login from './components/Login';
+import Home from './components/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <BrowserRouter>
+    <pageNav />
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={< Home />} />
+      <Route path="*" element={<pagenotFound/>} />
+    </Routes>
+  </BrowserRouter>
+  </div>
   );
 }
-
 export default App;
